0f988e43e3a27df89dea0c11bda47babd8aa3820
Asya'ri <asyari818@gmail.com>
2023-07-30 16:51:26 +0700

    fix: change copyright text

--- resources/views/components/footer-landing.blade.php
+++ resources/views/components/footer-landing.blade.php
@@ -30,7 +30,7 @@
     </div>
     <div class="footer footer-center p-4 bg-base-300 text-base-content">
         <div>
-            <p>Copyright © 2023 - All right reserved by Asya'ri & Nadhir Adhitya Z</p>
+            <p>© by Asya'ri & Nadhir Adhitya Z 2023</p>
         </div>
     </div>
 </footer>
