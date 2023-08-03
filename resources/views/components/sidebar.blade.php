0548fe4d6d6b070ff6b5abdac902abc95f3147fa
Asya'ri <asyari818@gmail.com>
2023-08-03 14:33:29 +0700

    feat: add menu bookpdf in sidebar dash

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,11 +1,25 @@
 <div class="w-full sm:w-1/6">
     <ul class="menu bg-base-200 sm:h-screen rounded-box">
         <li>
-            <x-dash-nav-link href="{{ route('book.index')}}" :active="request()->routeIs('book.index')">
-                <x-fas-book class="h-5 w-5" />Book
-            </x-dash-nav-link>
-            <x-dash-nav-link href="{{ route('content.index')}}" :active="request()->routeIs('content.index')">
-                <x-fas-book-open class="h-5 w-5" />Content
+            <details open>
+                <summary>Audiobook</summary>
+                <ul>
+                    <li>
+                        <x-dash-nav-link href="{{ route('book.index') }}" :active="request()->routeIs('book.index')">
+                            <x-fas-book class="h-5 w-5" />Book
+                        </x-dash-nav-link>
+                    </li>
+                    <li>
+                        <x-dash-nav-link href="{{ route('content.index') }}" :active="request()->routeIs('content.index')">
+                            <x-fas-book-open class="h-5 w-5" />Content
+                        </x-dash-nav-link>
+                    </li>
+                </ul>
+            </details>
+        </li>
+        <li>
+            <x-dash-nav-link href="{{ route('bookpdf.index') }}" :active="request()->routeIs('bookpdf.index')">
+                <x-fas-book class="h-5 w-5" />Book PDF
             </x-dash-nav-link>
         </li>
     </ul>
